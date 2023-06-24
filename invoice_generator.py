5150c03481173082a7a09a2366999ddc3d8c3fb0
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2023-06-24 10:09:16 +0100

    variables definition

---
+++ invoice_generator.py
@@ -0,0 +1,46 @@
+'''
+INVOICE GENERATOR
+Project by Alessandro Silvestri 2023
+'''
+
+'''
+##### INPUT ##################################################
+
+invoice_number
+date
+currency
+invoice_from
+invoice_to
+
+INPUT list
+
+row[
+{
+description: "describe the job done",
+quantity: <1>,
+rate: <price value>
+amount: <amount value value showing the currency>
+}
+]
+
+notes
+
+
+##### OUTPUT ##################################################
+
+
+invoice_number
+date
+invoice_from
+invoice_to
+
+row[
+{
+description: "description",
+quantity: <quantity>,
+rate: <value>
+amount: <amount value value showing the currency>
+]
+}
+
+'''
